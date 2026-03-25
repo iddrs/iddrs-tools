0adcda078bd8a0e4504ce69140890f51cc040365
everton3x <everton3x@gmail.com>
2026-03-25 08:49:09 -0300

    Adicionado teste do duodécimo x dotação atualizada.

---
+++ app/contab/tester/tests/duodecimo-dotacao.php
@@ -0,0 +1,54 @@
+<?php
+
+use Twig\Environment;
+use function contab\tester\get_value_for_test;
+use function contab\tester\test_name;
+
+return function(int $remessa, string $entidade, Environment $templates, array &$result): bool {
+    
+    $name = 'Duodécimo total x Dotação atualizada';
+    $testId = test_name(__FILE__);
+    $success = false;
+    
+    $etotal = 0.0;
+    $dtotal = 0.0;
+    
+    $sql = <<<SQL
+        select sum(saldo_final)
+        from bal_ver 
+        where remessa = $remessa and entidade like 'cm' 
+            and conta_contabil like '4.9.9.9.2.01.%';
+    SQL;
+    $valor = get_value_for_test($sql);
+    $etotal += $valor;
+    $esquerdo[$sql] = $valor;
+    
+    $exercicio = substr($remessa, 0, 4);
+    $sql = <<<SQL
+        select sum(dotacao_atualizada) 
+        from bal_desp
+        where remessa = $remessa and entidade like 'cm'
+            
+    SQL;
+    $valor = get_value_for_test($sql);
+    $dtotal += $valor;
+    $direito[$sql] = $valor;
+    
+    
+    
+    if(round($etotal, 2) === round($dtotal, 2)) $success = true;
+    
+    $result[$testId] = [
+        'name' => $name,
+        'success' => $success,
+        'html' => $templates->render('esquerdo-direito.twig', [
+            'testId' => $testId,
+            'testTitle' => $name,
+            'testSuccess' => $success,
+            'esquerdo' => $esquerdo,
+            'direito' => $direito,
+        ])
+    ];
+    
+    return $success;
+};
